1634625f47ee9aaffdda68ac2e47d9c8a95fa3e6
Vlad Paulechka <vlad9.ru@gmail.com>
2022-12-13 14:37:42 +0300

    убрана кнопка закрыть1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
 
 const Header = () => {
-    const {user, onClose} = useTelegram()
     return (
         <div className={'header'}>
             <span className={'username'}>
